ae05e3e058340fbabae560c731e51088d4bfa3fd
Jordan Olsen <jolsen@neslotech.co.za>
2024-07-30 14:32:23 +0200

    SPY: create banner component

---
+++ app/component/banner/Banner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './banner.scss';
+
+export const Banner = () => (
+    <section className="banner">
+        <h1 className="typescale-10">Listen to your mother</h1>
+    </section>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { Hero } from './component/hero/Hero';
+import { Banner } from "./component/banner/Banner";
 
 const Home = () => (
   <main>
     <Hero />
-    <h1>Test this</h1>
+    <Banner />
   </main>
 );
 
