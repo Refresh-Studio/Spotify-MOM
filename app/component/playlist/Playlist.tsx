1f4d37bc6f0b9661169191f71fe4dc1f92f59092
Jordan Olsen <jolsen@neslotech.co.za>
2024-07-30 14:36:45 +0200

    SPY: create playlist component

---
+++ app/component/playlist/Playlist.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './playlist.scss';
+
+export const Playlist = () => (
+    <section className="playlist">
+        <h1>Playlist goes here</h1>
+    </section>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Hero } from './component/hero/Hero';
 import { Banner } from "./component/banner/Banner";
+import { Playlist } from "./component/playlist/Playlist";
 
 const Home = () => (
   <main>
     <Hero />
     <Banner />
+      <Playlist />
   </main>
 );
 
