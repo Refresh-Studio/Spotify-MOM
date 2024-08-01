ec36e19b465d11ca25095358ee02c49fee2658e3
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-01 21:21:59 +0200

    SPY: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-'use client';
-
+import { Metadata, Viewport } from 'next';
+import Head from 'next/head';
 import React, { PropsWithChildren } from 'react';
 
 import { Footer } from './component/footer/Footer';
@@ -9,10 +9,20 @@ import { defaultFont } from './constant';
 
 import './index.scss';
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1
+};
+
+export const metadata: Metadata = {
+  title: 'Spotify | MOM',
+  description: "Get ready for a special announcement from Spotify Africa: 'Mom is Coming Home'."
+};
+
 const RootLayout = ({ children }: Readonly<PropsWithChildren>) => (
   <html lang="en">
     <body className={defaultFont.className}>
-      <script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
       <Header />
       {children}
       <Footer />
