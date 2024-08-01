243f66d5ad4e2efe4999aa750f02c9ae19bd2abc
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-01 20:24:22 +0200

    SPY: remove playlist integrations

--- app/component/playlist/Playlist.tsx
+++ app/component/playlist/Playlist.tsx
@@ -1,105 +1,31 @@
 'use client';
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
-import { TrackDetails } from './track.interface';
+import React from 'react';
 
 import { ReactComponent as ViewIcon } from '../../asset/view.svg';
 
-import { Track } from './track/Track';
-
 import './playlist.scss';
 
-declare const window: any;
-
-const getPlaylist = async (accessToken: string) => {
-  const { data } = await axios.get(
-    'https://api.spotify.com/v1/playlists/37i9dQZF1DWWuGghJUW6Hp?market=ZA',
-    {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    }
-  );
-
-  return data.tracks.items;
-};
-
-interface Props {
-  accessToken?: string;
-}
-
-export const Playlist = ({ accessToken }: Props) => {
-  const [API, setAPI] = useState<any>();
-  const [controller, setController] = useState<any>();
-  const [tracks, setTracks] = useState<TrackDetails[]>([]);
-  const [playingTrack, setPlayingTrack] = useState<string>();
-
-  useEffect(() => {
-    window.onSpotifyIframeApiReady = (IFrameAPI: any) => {
-      setAPI(IFrameAPI);
-    };
-  }, []);
-
-  useEffect(() => {
-    const callApi = async () => {
-      const tracks = await getPlaylist(accessToken!);
-      setTracks(tracks);
-    };
-
-    if (accessToken) {
-      callApi();
-    }
-  }, [accessToken]);
-
-  const handlePlay = (uri: string) => {
-    if (controller) {
-      handleStop();
-    }
-
-    const element = document.getElementById('playback');
-    const playbackElement = document.createElement('div');
-    element!.appendChild(playbackElement);
-
-    const options = {
-      width: '100%',
-      height: '100%',
-      background: 'black',
-      uri
-    };
-    const callback = (EmbedController: any) => {
-      setController(EmbedController);
-      EmbedController.play();
-    };
-    API.createController(playbackElement, options, callback);
-    setPlayingTrack(uri);
-  };
-
-  const handleStop = () => {
-    controller.destroy();
-    setPlayingTrack(undefined);
-  };
-
-  return (
-    <section className="playlist">
-      <div className="playlist__tracks">
-        <iframe
-          style={{ borderRadius: 12 }}
-          src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWuGghJUW6Hp?utm_source=generator&theme=0"
-          width="100%"
-          height="746"
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
-      </div>
-      <footer>
-        <a
-          className="typescale-4"
-          href="https://open.spotify.com/playlist/37i9dQZF1DWWuGghJUW6Hp"
-          target="_blank"
-        >
-          View Playlist <ViewIcon />
-        </a>
-      </footer>
-    </section>
-  );
-};
+export const Playlist = () => (
+  <section className="playlist">
+    <div className="playlist__tracks">
+      <iframe
+        style={{ borderRadius: 12 }}
+        src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWuGghJUW6Hp?utm_source=generator&theme=0"
+        width="100%"
+        height="746"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
+    <footer>
+      <a
+        className="typescale-4"
+        href="https://open.spotify.com/playlist/37i9dQZF1DWWuGghJUW6Hp"
+        target="_blank"
+      >
+        View Playlist <ViewIcon />
+      </a>
+    </footer>
+  </section>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,19 @@
 'use client';
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { Banner } from './component/banner/Banner';
-import { Footer } from './component/footer/Footer';
 import { Hero } from './component/hero/Hero';
 import { Playlist } from './component/playlist/Playlist';
 import { Subscription } from './component/subscription/Subscription';
 
-const getData = async () => {
-  const authToken = Buffer.from(
-    `${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`
-  ).toString('base64');
-  const { data } = await axios.post<{ access_token: string }>(
-    'https://accounts.spotify.com/api/token?scope=web-playback',
-    { grant_type: 'client_credentials' },
-    {
-      headers: {
-        Authorization: `Basic ${authToken}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    }
-  );
-
-  return { accessToken: data.access_token };
-};
-
-const Home = () => {
-  const [accessToken, setAccessToken] = useState<string>();
-
-  useEffect(() => {
-    const callApi = async () => {
-      const { accessToken } = await getData();
-      setAccessToken(accessToken);
-    };
-
-    callApi();
-  }, []);
-
-  return (
-    <main>
-      <Hero />
-      <Banner />
-      <Playlist accessToken={accessToken} />
-      <Subscription />
-    </main>
-  );
-};
+const Home = () => (
+  <main>
+    <Hero />
+    <Banner />
+    <Playlist />
+    <Subscription />
+  </main>
+);
 
 export default Home;
