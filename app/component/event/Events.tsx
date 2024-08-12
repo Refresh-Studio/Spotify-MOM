60e7ba3eda95b45fa8f508e430993df10d89e10f
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-12 10:40:50 +0200

    SPY: update hero content

---
+++ app/component/event/Events.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Events = () => <section className="events"></section>;
--- app/component/hero/Hero.tsx
+++ app/component/hero/Hero.tsx
@@ -13,23 +13,15 @@ export const Hero = () => (
       <span>MOM: Mother of Music</span>
       <span>5,8 Sep, Cape Town</span>
     </div>
-    <div>
-      <span>Listen to your mother</span>
-      <span>
-        Scroll down <DownIcon />
-      </span>
-    </div>
     <div className="hero__content">
       <p className="typescale-9">
         Mother of Music is a 4-day Discovery of Emerging Sounds and Art from Africa in the Mother
         City.
       </p>
-      <p className="typescale-9">Join MOM as she spotlights the african renaissance.</p>
+      <p className="typescale-9">Join MOM as she spotlights the African Renaissance.</p>
     </div>
     <footer>
-      <span className="typescale-6">Cape Town</span>
       <BannerIcon />
-      <span className="typescale-6">5,8 Sep, Cape Town</span>
     </footer>
   </section>
 );
