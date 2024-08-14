1deab244328fa58dc69c216087423ead16c94e6e
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-14 08:51:24 +0200

    SPY: update banner etc

--- app/(site)/component/hero/Hero.tsx
+++ app/(site)/component/hero/Hero.tsx
@@ -5,6 +5,6 @@ import './hero.scss';
 export const Hero = () => (
   <section className="hero dark-section">
     <span className="typescale-6">MOM: Mother of Music</span>
-    <span className="typescale-6">5,8 Sep, Cape Town</span>
+    <span className="typescale-6">5-8 Sep, Cape Town</span>
   </section>
 );
