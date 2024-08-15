38f24081a3bc0fa884ef23281df7f2194106170c
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-15 13:28:06 +0200

    SPY: fix register button

--- app/(site)/tickets/component/tickets/Tickets.tsx
+++ app/(site)/tickets/component/tickets/Tickets.tsx
@@ -78,7 +78,6 @@ export const Tickets = ({ events }: Props) => {
                     />
                   ) : (
                     <Button
-                      clickable
                       hollow
                       large
                       label="Register for Tickets"
