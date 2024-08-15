65f7bac20201827ed168315a5fd1cd37bdd56365
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-15 10:24:05 +0200

    SPY: remove lottie

--- app/(site)/component/hero/Hero.tsx
+++ app/(site)/component/hero/Hero.tsx
@@ -1,14 +1,11 @@
-import Lottie from 'lottie-react';
 import React from 'react';
 
 import { wideFont } from '../../../constant';
-import data from './animation.json';
 
 import './hero.scss';
 
 export const Hero = () => (
   <section className="hero dark-section">
-    <Lottie animationData={data} loop />
     <span className={`typescale-4 ${wideFont.className}`}>MOM: Mother of Music</span>
     <span className={`typescale-4 ${wideFont.className}`}>5 - 8 September, Cape Town</span>
   </section>
