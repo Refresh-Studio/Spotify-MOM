2e97adc45ec5fa9d9a46c19a1eee3632fd6f00fe
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-15 14:44:16 +0200

    SPY: set redirect to HTTPS

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://www.spotifymom.com/:path*',
+        permanent: true
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
