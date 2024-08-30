8ecd2d278c1f109e6970c2ad40d5529312a332e4
Jordan Olsen <jolsen@neslotech.co.za>
2024-08-30 23:11:14 +0200

    fix support email

--- app/(site)/component/modal/Modal.tsx
+++ app/(site)/component/modal/Modal.tsx
@@ -14,48 +14,46 @@ import './modal.scss';
 
 interface Props {
   open: boolean;
+  // eslint-disable-next-line no-unused-vars
   setOpen: (open: boolean) => void;
   promotion: Promotion;
 }
 
-export const Modal = ({ open, setOpen, promotion }: Props) => {
-  console.log(promotion);
-  return (
-    <Dialog open={open} onClose={() => {}} className="modal">
-      <div>
-        <DialogPanel className="modal__panel">
-          <div className="modal__details">
-            <Image src={promotion?.image} alt={promotion?.event?.name} width={184} height={230} />
-            <div>
-              <div className="modal__details-event">
-                <p className="typescale-2">Register for free tickets</p>
-                <h1 className={`typescale-5 ${wideFont.className}`}>{promotion?.event?.name}</h1>
-                <h2 className={`typescale-5 ${wideFont.className}`}>{promotion?.event?.address}</h2>
-              </div>
-              <div className="modal__times">
-                <p className="typescale-2">{promotion?.event?.startDate}</p>
-                <p className="typescale-2">
-                  {promotion?.event?.startTime} - {promotion?.event?.endTime}
-                </p>
-              </div>
+export const Modal = ({ open, setOpen, promotion }: Props) => (
+  <Dialog open={open} onClose={() => {}} className="modal">
+    <div>
+      <DialogPanel className="modal__panel">
+        <div className="modal__details">
+          <Image src={promotion?.image} alt={promotion?.event?.name} width={184} height={230} />
+          <div>
+            <div className="modal__details-event">
+              <p className="typescale-2">Register for free tickets</p>
+              <h1 className={`typescale-5 ${wideFont.className}`}>{promotion?.event?.name}</h1>
+              <h2 className={`typescale-5 ${wideFont.className}`}>{promotion?.event?.address}</h2>
+            </div>
+            <div className="modal__times">
+              <p className="typescale-2">{promotion?.event?.startDate}</p>
+              <p className="typescale-2">
+                {promotion?.event?.startTime} - {promotion?.event?.endTime}
+              </p>
             </div>
           </div>
-          <div className="modal__actions">
-            <div onClick={() => setOpen(false)}>
-              <CloseIcon />
-            </div>
-            {promotion?.event?.quicketEventId && (
-              <Link
-                href={promotion?.event?.quicketEventId}
-                target="_blank"
-                onClick={() => setOpen(false)}
-              >
-                <RightIcon />
-              </Link>
-            )}
+        </div>
+        <div className="modal__actions">
+          <div onClick={() => setOpen(false)}>
+            <CloseIcon />
           </div>
-        </DialogPanel>
-      </div>
-    </Dialog>
-  );
-};
+          {promotion?.event?.quicketEventId && (
+            <Link
+              href={promotion?.event?.quicketEventId}
+              target="_blank"
+              onClick={() => setOpen(false)}
+            >
+              <RightIcon />
+            </Link>
+          )}
+        </div>
+      </DialogPanel>
+    </div>
+  </Dialog>
+);
