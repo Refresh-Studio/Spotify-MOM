cbcfee29d764fb3482045b50bcf12d1d7af4e473
Tristan Carswell <tcarswell@neslotech.co.za>
2024-09-09 15:26:29 +0200

    SPY-3: Fix formatting

--- app/(site)/artists/component/hero/Hero.tsx
+++ app/(site)/artists/component/hero/Hero.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+
 import { wideFont } from '../../../../constant';
 
 import './hero.scss';
 
 interface Props {
-    artistAmount: number;
+  artistAmount: number;
 }
 
 export const Hero = ({ artistAmount }: Props) => (
-    <section className="artists-hero light-section">
-        <h1 className={`typescale-10 ${wideFont.className}`}>Artists</h1>
-        <div className="counter">
-            <span className="typescale-5">{artistAmount}</span>
-        </div>
-    </section>
+  <section className="artists-hero light-section">
+    <h1 className={`typescale-10 ${wideFont.className}`}>Artists</h1>
+    <div className="counter">
+      <span className="typescale-5">{artistAmount}</span>
+    </div>
+  </section>
 );
