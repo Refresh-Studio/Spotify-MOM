a21a6e8a87e19e2dd308f64f87384add806bae57
SabeloX <sabelomtw@gmail.com>
2024-09-10 14:22:32 +0200

    SPY-9: add display buttons and update hero theme

--- app/(site)/albums/component/hero/Hero.tsx
+++ app/(site)/albums/component/hero/Hero.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ReactComponent as GridIcon } from '../../../../asset/grid.svg';
+import { ReactComponent as ListIcon } from '../../../../asset/list.svg';
 
 import { wideFont } from '../../../../constant';
 
 import './hero.scss';
 
-export const Hero = () => (
-  <section className="gallery-hero dark-section">
-    <h1 className={`typescale-10 ${wideFont.className}`}>Gallery</h1>
-    <p className="typescale-3">Relive the magic of MOM through her lens.</p>
-  </section>
-);
+interface Props {
+  albumCount: number;
+}
+
+type DisplayState = 'grid' | 'list';
+
+export const Hero = ({ albumCount }: Props) => {
+  const [displayState, setDisplayState] = useState<DisplayState>('grid');
+
+  const handleDisplayState = (state: DisplayState) => {
+    setDisplayState(state);
+  };
+
+  return (
+    <section className="gallery-hero dark-section">
+      <div>
+        <h1 className={`typescale-10 ${wideFont.className}`}>
+          Gallery
+          <span className="typescale-5">{albumCount}</span>
+        </h1>
+        <div>
+          <span
+            className={`gallery-hero__icon ${displayState === 'grid' ? 'gallery-hero__icon--active' : ''}`}
+            onClick={() => handleDisplayState('grid')}
+          >
+            <GridIcon />
+          </span>
+          <span
+            className={`gallery-hero__icon ${displayState === 'list' ? 'gallery-hero__icon--active' : ''}`}
+            onClick={() => handleDisplayState('list')}
+          >
+            <ListIcon />
+          </span>
+        </div>
+      </div>
+      <hr />
+    </section>
+  );
+};
