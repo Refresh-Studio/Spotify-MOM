7a582d9da635c61e3166e621e7e92605141e3919
SabeloX <sabelomtw@gmail.com>
2024-09-10 14:42:10 +0200

    SPY-7: only display [?1049h[>4;2m[?1h=[?2004h[?1004h[1;15r[?12h[?12l[22;2t[22;1t[27m[23m[29m[m[H[2J[?25l[15;1H"Gallery" [New][2;1H[94m~                                                                 [3;1H~                                                                 [4;1H~                                                                 [5;1H~                                                                 [6;1H~                                                                 [7;1H~                                                                 [8;1H~                                                                 [9;1H~                                                                 [10;1H~                                                                 [11;1H~                                                                 [12;1H~                                                                 [13;1H~                                                                 [14;1H~                                                                 [1;1H[?25h[?4m[15;1H[?2004l[>4;m[23;2t[23;1t[m[15;1H[K[15;1H[?1004l[?2004l[?1l>[?1049l[>4;m button

--- app/(site)/tickets/component/tickets/Tickets.tsx
+++ app/(site)/tickets/component/tickets/Tickets.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { ArrowTopRightIcon } from '@sanity/icons';
 import { useSearchParams } from 'next/navigation';
 import React, { useEffect, useMemo, useState } from 'react';
 
-import { formatGoogleCalendarDate, scrollIntoView } from '../../../../util';
+import { scrollIntoView } from '../../../../util';
 
 import { EventItem } from '../../../../interface/event/event-item.interface';
 import { Filter } from '../../../../interface/filter/filter.interface';
@@ -13,8 +12,6 @@ import { Album } from '../../../../interface/gallery/album.interface';
 import { Button } from '../../../component/button/Button';
 import { Event } from '../../../component/events/event/Event';
 import { TabItem, Tabs } from '../../../component/tabs/Tabs';
-
-import { ReactComponent as PlusIcon } from '../../../../asset/plus.svg';
 
 import { getAlbums, getEventFilters } from '../../../../../sanity/sanity.query';
 
@@ -76,17 +73,6 @@ export const Tickets = ({ events }: Props) => {
       <main>
         <ul>
           {filteredEvents.map((event: EventItem) => {
-            let calendarLink = 'https://www.google.com/calendar/render?action=TEMPLATE';
-            calendarLink += `&text=${encodeURIComponent(event.name)}`;
-            calendarLink += `&dates=${formatGoogleCalendarDate(event.calendarStartDate, event.startTime)}/`;
-            calendarLink += `${formatGoogleCalendarDate(event.calendarEndDate ?? event.calendarStartDate, event.endTime)}`;
-            calendarLink += `&details=${encodeURIComponent(
-              (event.description ?? [])
-                .flatMap((block) => block?.children)
-                .map((block) => block?.text)
-                .join('\n')
-            )}`;
-
             const albumSlug = albums.find((album) => album.event.slug === event.slug)?.slug;
 
             return (
@@ -99,24 +85,8 @@ export const Tickets = ({ events }: Props) => {
                 action={
                   albumSlug ? (
                     <Button light large link={`/albums/${albumSlug}`} label="View Gallery" />
-                  ) : event.free ? (
-                    <Button
-                      light
-                      large
-                      icon={<PlusIcon />}
-                      target="_blank"
-                      link={calendarLink}
-                      label="Add to Calendar"
-                    />
                   ) : (
-                    <Button
-                      light
-                      large
-                      icon={<ArrowTopRightIcon width={20} height={20} />}
-                      label="Register for Tickets"
-                      link={event.quicketEventId}
-                      target="_blank"
-                    />
+                    <></>
                   )
                 }
               />
