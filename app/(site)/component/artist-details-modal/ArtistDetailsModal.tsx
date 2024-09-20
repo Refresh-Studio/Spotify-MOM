c5a9994235ed795ce7f8e1af5599f3e63cd87fa6
SabeloX <sabelomtw@gmail.com>
2024-09-20 12:59:45 +0200

    stash

---
+++ app/(site)/component/artist-details-modal/ArtistDetailsModal.tsx
@@ -0,0 +1,37 @@
+import { Dialog, DialogPanel } from '@headlessui/react';
+import { ReactComponent as CloseIcon } from '../../../asset/close.svg';
+import { ResponsiveImage } from '../carousel/ResponsiveImage';
+
+import './artist-details-modal.scss';
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+}
+
+const name = 'Dee Koala';
+
+const description = 'Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent hendrerit vestibulum dolor donec pellentesque condimentum aenean. Porttitor enim ante dolor pharetra adipiscing rhoncus. Elit dictum gravida potenti ornare dis risus. Condimentum rutrum semper efficitur habitasse massa morbi potenti. Vel mollis efficitur, rhoncus sociosqu pulvinar facilisi class.';
+
+export const ArtistDetailsModal = ({ open, onClose }: Props) => (
+  <Dialog open={open} onClose={onClose} className={`artist-details-modal ${open ? 'artist-details-modal--open' : ''}`}>
+    <div>
+      <DialogPanel className="artist-details-modal__panel">
+        <div className="artist-details-modal__details">
+          <div className="artist-details-modal__close" onClick={onClose}>
+            <CloseIcon />
+          </div>
+          <div className="artist-details-modal__content">
+            <ResponsiveImage src="/images/background.jpg" alt="" />
+            <div>
+              <h3 className="typescale-6">{name}</h3>
+              <p className="typescale-5">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </div>
+  </Dialog>
+);
--- app/(site)/component/banner/Banner.tsx
+++ app/(site)/component/banner/Banner.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { wideFont } from '../../../constant';
 
 import './banner.scss';
+import { ArtistDetailsModal } from '../artist-details-modal/ArtistDetailsModal';
 
-export const Banner = () => (
-  <section className="banner">
-    <div>
-      <h1 className={`typescale-8 ${wideFont.className}`}>
-        DISCOVER. EMERGING. <br /> COMMUNITY.
-      </h1>
-    </div>
-  </section>
-);
+export const Banner = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  // const isDesktop = useMemo(() => window.screen.width >= 1024, []);
+
+  return (
+    <section className="banner" onClick={true ? (() => setOpen(true)) : () => {}}>
+      {true &&
+        <ArtistDetailsModal open={open} onClose={() => setOpen(false)} />}
+      <div>
+        <h1 className={`typescale-8 ${wideFont.className}`}>
+          DISCOVER. EMERGING. <br /> COMMUNITY.
+        </h1>
+      </div>
+    </section>
+  );
+};
