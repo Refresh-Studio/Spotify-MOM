9f4b49a426ae0edd87bfdd84dca0cc87a4a24360
Tristan Carswell <tcarswell@neslotech.co.za>
2024-09-24 15:00:10 +0200

    SPY-42: Fix spacing on the tickets page, align to design

--- app/(site)/tickets/component/tickets/Tickets.tsx
+++ app/(site)/tickets/component/tickets/Tickets.tsx
@@ -105,7 +105,7 @@ export const Tickets = ({ events }: Props) => {
   };
 
   return (
-    <section className="tickets dark-section">
+    <section className="dark-section">
       <Tabs tabs={filters} hollow />
       <main>
         <ul>
